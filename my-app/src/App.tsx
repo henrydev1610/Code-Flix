1fb5b2a4ed74cb44c02b65e1ce6dea2b17bc2acd
Henry Benjamin <henrybenjamin.dev@gamil.com>
2024-02-15 13:54:04 -0300

    criando barra superior

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,17 @@
 import {Box ,ThemeProvider} from '@mui/system'
 import Header from './Components/Header'
+import { createTheme } from '@mui/material'
+
+
+
+
+
+const theme = createTheme({})
+
 
 const App = ()=>{
   return(
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme}>
         
       <Box
         component='main'
