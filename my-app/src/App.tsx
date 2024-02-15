62a2647b6c69ae278b7bcb08a37489a5db19fbcb
Henry Benjamin <henrybenjamin.dev@gamil.com>
2024-02-15 12:32:08 -0300

    Primeiro commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained">Saiba mais </Button>
+    </div>
+  )
+}
+
+export default App
